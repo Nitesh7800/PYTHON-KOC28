7b83c2e1ba77589bf67e275cf760c1fcabb9b582
Nitesh Pathak <niteshpathak1462@gmail.com>
2022-10-16 13:23:59 +0530

    done

---
+++ randomNum.py
@@ -0,0 +1,16 @@
+a=int(input("enter  first number"))
+b=int(input("enter second number"))
+c=str(input("what do you want to do"))
+if c=="add":
+    print("answer is ,a+b")
+elif c=="subtract": 
+    print("answer is ",a-b)
+elif c=="multiply":
+    print("answer is",a-b)
+elif c=="divide":
+    print("answer is",a/b)
+else :
+    print("Invalid")
+
+
+
