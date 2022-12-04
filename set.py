bf88068b6902171e2f6eb2318c2e1fa0c1b94ce6
Nitesh Pathak <114794954+Nitesh7800@users.noreply.github.com>
2022-12-04 15:33:15 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,12 @@
+myset={"car","boat","train"}
+#for i in myset:
+ #   if i=="boat":
+ #      print(i)
+
+#myset2=={1,2,3}
+#myset2.add("car")
+#print(myset2)
+
+myset1=("car","bull","train")
+x=myset.intersection(myset1)
+print(x)
